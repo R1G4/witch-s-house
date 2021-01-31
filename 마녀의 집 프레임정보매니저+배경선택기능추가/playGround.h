b866d2b8bae79fbf0fe14001904f19a558a643d6
Hangoms <huny0524@gmail.com>
2021-02-01 02:01:13 +0900

    배경선택기능까지 추가했
    그랬음

---
+++ 마녀의 집 프레임정보매니저+배경선택기능추가/playGround.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "gameNode.h"
+#include "mapTool.h"
+#include "garDen.h"
+#include "stage1_1.h"
+
+class playGround : public gameNode
+{
+private:
+	float alpha;
+	int _currentFrameX;
+	int _currentFrameY;
+	int count;
+	POINT pt;
+	Image* viola;
+	POINT camera;
+public:
+	playGround();
+	~playGround();
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+	void addBackGroundImage();
+};
+
